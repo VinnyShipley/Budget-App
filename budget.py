a591cd81b77f1e5b06ea2f904f80995c7ce36762
Vinny <vinnyshipley@gmail.com>
2023-07-26 04:04:14 +0000

    "Got the one expense bug working with a refactor"

--- budget.py
+++ budget.py
@@ -21,50 +21,52 @@ def income_collector():
 # Creating input for user to enter expenses
 def expense_collector(income_list_input):
   j = 0
+  expense_list = [0] * len(income_list_input)
   for income in income_list_input:
+    expenses_for_this_income = 0
     expense_list_length = input(f"How many expenses are you calculating today for income {j + 1}> ")
-    expense_list = [0] * int(expense_list_length)
-    j += 1
     
   
     # Collects expenses
     i = 0
-    while i + 1 <= len(expense_list):
+    while i + 1 <= len(expense_list_length):
       expense_list_value = input(f"Expense # {i + 1} cost in dollars: > ")
-      expense_list[i] = int(expense_list_value)
+      expenses_for_this_income += int(expense_list_value)
       i += 1
-  
+    expense_list[j] = expenses_for_this_income
+    j += 1
   return expense_list
 
 
 
 
 def budget_splitter(list_of_incomes_final, list_of_expenses_per_income_final):
-  list_of_incomes_after_expenses = [0] * len(list_of_incomes_final)
-  
-  
-  #Loop that subtracts expenses from the correct income and returns list of incomes after expenses
-  j = 0
-  for income in list_of_incomes_final:
-    money_after_expenses = list_of_incomes_final[j] - list_of_expenses_per_income_final[j]
-    list_of_incomes_after_expenses[j] = money_after_expenses
-    j += 1
-  
-  #Creates and displays the combined money after expenses
-  combined_spending_money = sum(list_of_incomes_after_expenses)
-  print(f"The combined remaining money for fun expenses after bills is {combined_spending_money}")
-
-  
-  
-  #Gathers percentages of spending money per incomes after expenses
+  incomes_after_expenses = []
   i = 0
-  while i < len(list_of_incomes_final):
-    precise_percent_person_pays =  list_of_incomes_after_expenses[i] / combined_spending_money
-    rounded_percent_person_pays = 100 * (round(precise_percent_person_pays, 2))
-    
-    print(f"The person with the income of {list_of_incomes_final[i]} will pay {rounded_percent_person_pays} % of the expenses after bills this month.")
-
+  for income in list_of_incomes_final:
+    income_after_expenses = income - list_of_expenses_per_income_final[i]
+    incomes_after_expenses.append(income_after_expenses)
     i += 1
+  print(f"the incomes after expenses are {incomes_after_expenses}")
+  
+  
+
+  
+  # #Creates and displays the combined money after expenses
+  # combined_spending_money = sum(list_of_incomes_after_expenses)
+  # print(f"The combined remaining money for fun expenses after bills is {combined_spending_money}")
+
+  
+  
+  # #Gathers percentages of spending money per incomes after expenses
+  # i = 0
+  # while i < len(list_of_incomes_final):
+  #   precise_percent_person_pays =  list_of_incomes_after_expenses[i] / combined_spending_money
+  #   rounded_percent_person_pays = 100 * (round(precise_percent_person_pays, 2))
+    
+  #   print(f"The person with the income of {list_of_incomes_final[i]} will pay {rounded_percent_person_pays} % of the expenses after bills this month.")
+
+  #   i += 1
 
 
 incomes = income_collector()
