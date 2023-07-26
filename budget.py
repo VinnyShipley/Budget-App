92b6d102f75668b6789cc5db0fa205384d029b07
Vinny <vinnyshipley@gmail.com>
2023-07-26 01:46:13 +0000

    "changed the math so that percents make sense"

--- budget.py
+++ budget.py
@@ -56,7 +56,7 @@ def budget_splitter(list_of_incomes_final, list_of_expenses_per_income_final):
   #Gathers percentages of spending money per incomes after expenses
   i = 0
   while i <= len(list_of_incomes_final):
-    percent_person_pays = combined_spending_money / list_of_incomes_after_expenses[i]
+    percent_person_pays =  list_of_incomes_after_expenses[i] / combined_spending_money
     print(f"The person with the income of {list_of_incomes_final[i]} will pay {percent_person_pays} % of the expenses after bills this month.")
     i += 1
   
