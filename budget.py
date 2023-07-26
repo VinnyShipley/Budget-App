f6fc65df85eb390145df7d398a8beb3efefaacf7
Vinny <vinnyshipley@gmail.com>
2023-07-26 04:16:02 +0000

    "Readability"

--- budget.py
+++ budget.py
@@ -24,7 +24,7 @@ def expense_collector(income_list_input):
   expense_list = [0] * len(income_list_input)
   for income in income_list_input:
     expenses_for_this_income = 0
-    expense_list_length = input(f"How many expenses are you calculating today for income {j + 1}> ")
+    expense_list_length = input(f"How many expenses are you calculating today for income {j + 1} > ")
     
   
     # Collects expenses
@@ -64,7 +64,7 @@ def budget_splitter(list_of_incomes_final, list_of_expenses_per_income_final):
     precise_percent_person_pays =  incomes_after_expenses[i] / combined_spending_money
     rounded_percent_person_pays = 100 * (round(precise_percent_person_pays, 2))
     
-    print(f"The person with the income of {list_of_incomes_final[i]} will pay {rounded_percent_person_pays} % of the expenses after bills this month.")
+    print(f"The person with the income before expenses of {list_of_incomes_final[i]} will pay {rounded_percent_person_pays} % of the expenses after bills this month.")
 
     i += 1
 
